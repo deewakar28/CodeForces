0b8bbefa5946f54a5a39abd22fdd62da22271916
B. Deewakar Rao <deewakarrao59@gmail.com>
2024-01-14 16:31:50 +0530

    69/A

---
+++ A_Young_Physicist.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int t;
+    cin>>t;
+
+    int x, y, z;
+    int sumx(0), sumy(0), sumz(0);
+
+    while(t--){
+        cin>>x>>y>>z;
+        sumx += x;
+        sumy += y;
+        sumz += z;
+    }
+
+    if(sumx==0 && sumy==0 && sumz==0){
+        cout<<"YES"<<endl;
+    }
+    else{
+        cout<<"NO"<<endl;
+    }
+    return 0;
+
+}
