177e36995c171b87706e31afb1758da75c148ec7
B. Deewakar Rao <deewakarrao59@gmail.com>
2024-01-14 15:17:55 +0530

    1370/A

---
+++ A_Maximum_GCD.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int calcGCD(int n, int m){
+    while(n>0 && m>0){
+        if(n>m) n = n % m;
+        else m = m%n;
+
+    }
+    if(n==0) return m;
+    else return n;
+    
+}
+
+int main(){
+
+    int t;
+    cin>>t;
+    while(t--){
+      int x;
+      cin>>x;
+
+      cout<<x/2<<endl;
+       
+    }
+    return 0;
+}
