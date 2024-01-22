984db6e47bb9eb6aba71b906223c4a1681b84c27
B. Deewakar Rao <deewakarrao59@gmail.com>
2024-01-22 16:19:11 +0530

    1921/B

--- B_Arranging_Cats.cpp
+++ B_Arranging_Cats.cpp
@@ -10,7 +10,7 @@ int main(){
         int n;
         cin>>n;
 
-        int count = 0;
+        int count1 = 0, count2=0;
 
         string s;
         cin>>s;
@@ -19,11 +19,14 @@ int main(){
 
         for(int i=0; i<n; i++){
             if(s[i]=='0' && f[i]=='1'){
-                count++;
+                count1++;
+            }
+            else if(s[i]=='1' && f[i]=='0'){
+                count2++;
             }
         }
 
-        cout<<count<<endl;
+        cout<<max(count1, count1+count2-count1)<<endl;
     }
     return 0;
 }
