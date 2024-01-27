df1e00b8bb2195fdfc383a34fa075564811fcf1d
B. Deewakar Rao <deewakarrao59@gmail.com>
2024-01-27 13:16:26 +0530

    1915/A

---
+++ B_Not_Quite_Latin_Square.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int t;
+    cin>>t;
+    
+    while(t--){
+        string a, b, c;
+        cin>>a>>b>>c;
+
+        for(int i=0; i<a.size(); i++){
+            if(a[i]=='?' || b[i]=='?' || c[i]=='?'){
+                
+            }
+        }
+    }
+    return 0;
+}
