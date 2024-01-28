c6035e441a2ea76c2f2ffd7c0ec2ccdc2e2ce5ec
B. Deewakar Rao <deewakarrao59@gmail.com>
2024-01-28 16:50:27 +0530

    1917/B

--- A_Least_Product.cpp
+++ A_Least_Product.cpp
@@ -1,22 +1,33 @@
 #include<iostream>
-#include<vector>
 using namespace std; 
 
-int main(){
+#define ll long long int
+#define M INT_MAX
 
+void solve(){
+
+    ll n,a, prod=1;
+    cin>>n;
+
+    for(ll i=0; i<n; i++){
+        cin>>a;
+        
+        prod = (prod*a) % M;
+    }
+    if(prod>0){
+        cout<<1<<endl;
+        cout<<1<<" "<<0<<endl;
+    }
+    else{
+        cout<<0<<endl;
+    }
+}
+
+int32_t main(){
     int t;
     cin>>t;
+
     while(t--){
-        int n,a;
-        cin>>n>>a;
-
-        vector<int> v;
-
-        for(int i=0; i<n; i++){
-            cin>>a;
-            v.push_back(a);
-        }
-        
+        solve();
     }
-    return 0;
 }
