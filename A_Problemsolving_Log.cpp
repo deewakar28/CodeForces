578f72c88e65d2e396f4f5dfac6d9fe210533188
B. Deewakar Rao <deewakarrao59@gmail.com>
2024-01-29 23:57:49 +0530

    1914/A

---
+++ A_Problemsolving_Log.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+#define ll long long int
+
+void solve(){
+    ll n, count=0;
+    cin>>n;
+
+    string s;
+    cin>>s;
+
+    vector <int> v(26, 0);
+
+    for(int i=0; i<n; i++){
+        v[s[i]-'A']++;
+    }
+
+    for(int i=0; i<s.size(); i++){
+        if(v[i]>=i+1){
+            count++;
+        }
+    }
+
+    cout<<count<<endl;
+}
+
+int32_t main(){
+
+    int t;
+    cin>>t;
+
+    while(t--){
+        solve();
+    }
+}
